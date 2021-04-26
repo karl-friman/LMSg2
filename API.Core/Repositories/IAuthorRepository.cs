63e504f93a8522fa1dd3bf88822d73f4d33db8ef
LostVirt <rsvirtest@hotmail.com>
2021-04-26 16:28:28 +0200

    Controller, Unit of work

---
+++ API.Core/Repositories/IAuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Core.Entities;
+
+namespace API.Core.Repositories
+{
+    public interface IAuthorRepository : BasicRepositoryFeatures
+    {
+        Task<IEnumerable<Author>> getAllAuthors();
+        Task<Author> getAuthor(int? Id);
+    }
+}
---
+++ API/Controllers/AuthorController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Core.Entities;
+using API.Core.Repositories;
+using API.Data.Data;
+using AutoMapper;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly DbContextAPI db;
+        private readonly IUnitOfWork uow;
+        private readonly IMapper mapper;
+
+        public AuthorController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            uow = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        // GET: api/Author
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await db.Authors.ToListAsync();
+        }
+
+        // GET: api/Author/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            var author = await db.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        // PUT: api/Author/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthor(int id, Author author)
+        {
+            if (id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Author
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Author>> PostAuthor(Author author)
+        {
+            db.Authors.Add(author);
+            await db.SaveChangesAsync();
+
+            return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+        }
+
+        // DELETE: api/Author/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var author = await db.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            db.Authors.Remove(author);
+            await db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return db.Authors.Any(e => e.Id == id);
+        }
+    }
+}
