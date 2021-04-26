d3bed8c87a90a19c7a4df4987610e4ce76a65172
Susanna Sjöberg <susanna.sjoberg@gmail.com>
2021-04-26 12:00:32 +0200

    Add entities

---
+++ API/Entities/Literature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Literature
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Author> Authors { get; set; }
+
+        public ICollection<Subject> Subjects { get; set; }
+
+        public Level Level { get; set; }
+    }
+}
