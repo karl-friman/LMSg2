d9b25462ba52fd4bc3eae1d05c3a55b4a730dbee
monotones1 <monotones@hotmail.com>
2021-05-14 10:26:15 +0200

    laptop transfer

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,26 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Core.ViewModels;
+using Web.Data.Data;
+using Core.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork uow;
+        private readonly IMapper mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<LMSUser> userManager, IUnitOfWork uow, IMapper mapper)
         {
             _logger = logger;
+            _userManager = userManager;
+            this.uow = uow;
+            this.mapper = mapper;
         }
 
-        public IActionResult Index()
+        // GET: Courses
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var courseList = await uow.CourseRepository.GetAll(includeAll: true);
+            var model = mapper.Map<IEnumerable<CourseViewModel>>(courseList);
+
+            CourseListViewModel courseIndexModel = new CourseListViewModel
+            {
+                Courses = model,
+            };
+
+            return View(courseIndexModel);
         }
 
         public IActionResult Privacy()
